8d8011885169f301e3424df8408da9b698cce4fc
fxnoob <fxnoob71@gmail.com>
2020-08-01 16:42:26 +0530

    Init

---
+++ constants.js
@@ -0,0 +1,9 @@
+const constants = {
+  appConfig: {
+    appName: "Voice Command",
+    key:
+      "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk+TqSLHv+GDByTpenRDE\n4JAetLxsh5QgUatHvIlUH5XCJoBFXXdVa8jVatM+OZXYhEPDzHlpSinJjGpPLy79\n+tBMr9kjAj4BC9T6yIeF6NIwJNsL32QD2JQlxXgPAKs9awdzEyvTEtzC07eFHfx1\nqeo9yQr9Lv93NON/LTBUINfRjZmgtkwIe2JXDWNB/Vm7wwCFkBknpRk04b418gzV\nNXOOb2IDlrg53hpkhQ+kgEE+UVZuj5pRFB+RBa4P7iKjeLGGsckCqQTM3zyYHwZp\nL+5hY0azaYHbFet1m/t6zBn2YWYCSWWsrSnbRoHYJnxlzm9WDIFjd3/wPssuS5pp\npwIDAQAB\n-----END PUBLIC KEY-----\n" // put extension key here if required which would only be used development mode
+  }
+};
+
+module.exports = constants;
